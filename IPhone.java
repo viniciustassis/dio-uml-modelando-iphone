756c985ee7453511139825b004f2c2cccffce1bd
Vinicius Tavares Assis <92310141+viniciustassis@users.noreply.github.com>
2024-09-30 19:52:26 -0300

    Create IPhone.java

---
+++ IPhone.java
@@ -0,0 +1,50 @@
+public class IPhone implements ReprodutorMusical, AparelhoTelefonico, NavegadorInternet {
+
+    // Métodos do Reprodutor Musical
+    @Override
+    public void tocar() {
+        System.out.println("Tocando música.");
+    }
+
+    @Override
+    public void pausar() {
+        System.out.println("Música pausada.");
+    }
+
+    @Override
+    public void selecionarMusica(String musica) {
+        System.out.println("Selecionando a música: " + musica);
+    }
+
+    // Métodos do Aparelho Telefônico
+    @Override
+    public void ligar(String numero) {
+        System.out.println("Ligando para " + numero);
+    }
+
+    @Override
+    public void atender() {
+        System.out.println("Atendendo chamada.");
+    }
+
+    @Override
+    public void iniciarCorreioVoz() {
+        System.out.println("Iniciando correio de voz.");
+    }
+
+    // Métodos do Navegador na Internet
+    @Override
+    public void exibirPagina(String url) {
+        System.out.println("Exibindo página: " + url);
+    }
+
+    @Override
+    public void adicionarNovaAba() {
+        System.out.println("Nova aba adicionada.");
+    }
+
+    @Override
+    public void atualizarPagina() {
+        System.out.println("Página atualizada.");
+    }
+}
